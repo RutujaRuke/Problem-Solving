ac87665386136ae6aa2f44c34d9dea270674e77b
Rutuja <rutuja.ruke2004@gmail.com>
2026-07-06 22:32:20 +0530

    changes done

---
+++ python/6-july-26.py
@@ -0,0 +1,34 @@
+name = "Rutuja"
+age = 22
+address = "pune"
+
+print (type(name))
+print (type(age))
+print (type(address))
+
+
+x = 10
+print(x)
+print (type(x))
+
+y = "hello"
+print(y)
+print (type(y))
+
+price = 10.5
+print(price)
+print (type(price))
+
+is_true = True
+print(is_true)
+print (type(is_true))
+
+num = 10
+text = str(num)
+print(text)
+print (type(text))
+
+a = 10
+b = 20
+sum = a ** b
+print(sum)
