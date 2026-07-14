af8d3d99153eafb21bf6e520935cff26e9bfa7af
Rutuja <rutuja.ruke2004@gmail.com>
2026-07-14 21:55:10 +0530

    changes done

---
+++ python/11-july-26.py
@@ -0,0 +1,8 @@
+name = "Rutuja"
+age = 22
+address = "pune"
+
+print(type(name))
+
+print(f"my name is {name} and i am {age} years old and i ive in {address}")
+
---
+++ python/13-july-26.py
@@ -0,0 +1,38 @@
+list = ('1', '12', '123')
+
+tuple = ['1', '23', '322']
+
+value = range(3, 10, 3)
+
+print(type(list))
+print(type(tuple))
+print(type(value))
+
+for i in value:
+    print(i)
+
+# -------- function -------------
+
+def name(name):
+    print(f"my name is {name}")
+
+value = name("Ritu")
+
+def add(a, b):
+    print(f"sum of {a} and {b} is {a + b}")
+
+value2 = add(10, 20)
+
+def create_user(name, role="user"):
+    print(f"user name {name} and role is {role}")
+
+v1 = create_user('Rutuja', 'admin')
+v2 = create_user('Rutuja') 
+
+def add_item(item, cart=[]):
+    card.append(item)
+    return cart
+
+print(add_item('apple'))
+print(add_item('banana'))
+print(add_item('cherry'))
