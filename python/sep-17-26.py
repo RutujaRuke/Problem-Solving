6169877fd7621463b625079ac4cc2d7848f0d37a
Rutuja <rutuja.ruke2004@gmail.com>
2026-09-17 22:32:16 +0530

    chnages done

---
+++ python/sep-17-26.py
@@ -0,0 +1,39 @@
+list = ["tuple", "list", "dictionary", "set"]
+print(list[0])
+
+#edit the list item
+list[0] = "Ritu"
+print(list)
+
+list = ["tuple", "list", "dictionary", "set"]
+print(list[0])
+print(list)
+
+# -------------------------------------
+
+tuple = ("tuple", "list", "dictionary", "set")
+print(tuple[0])
+
+# -----------------------------------------
+
+dictionary = {"tuple": 1, "list": 2, "dictionary": 3, "set": 4}
+print(dictionary["tuple"])
+
+# ---------------------------------------
+
+set = {"tuple", "list", "dictionary", "set"}
+print(set)
+
+# ---------------------------------------
+
+set = {"tuple", "list", "dictionary", "set"}
+set.add("pari")
+print(set)
+set.add("Rituja")
+print(set)
+
+# ---------------------------------------
+
+set = {"tuple", "list", "dictionary", "set"}
+set.remove("list")
+print(set)
