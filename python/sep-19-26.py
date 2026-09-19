b7ff415b01c67264946150559161b2b62155717f
Rutuja <rutuja.ruke2004@gmail.com>
2026-09-19 22:21:44 +0530

    chnages done

---
+++ python/sep-19-26.py
@@ -0,0 +1,30 @@
+price = 100
+quantity = 5
+
+print(f"total price is: {price * quantity}")
+print(f"price is: {price}, quantity is: {quantity}")
+
+a = 10
+b = 30
+print(f"sum of {a} and {b} is: {a + b}")
+
+# --------------------
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+fruits.append("orange")
+for fruit in fruits:
+    print(fruit)
+flowers = ["rose", "lily", "jasmine"]
+
+# flowers = flower.objects.all()
+# for flower in flowers:
+    # print(flower)
+
+colors = ("red", "green")
+
+for color in colors:
+    print(color[0])
+    
